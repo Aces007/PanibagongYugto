494bf7e14f0ad2e4120ebf218bc7e0766e7cdf6c
Ace Lawrence <clavanoace@gmail.com>
2021-11-16 18:56:16 -0800

    Yehey, ayos na hahaha

--- GradingSystem.py
+++ GradingSystem.py
@@ -19,34 +19,34 @@ print(int(GradeMoRoundedOff))
 
 # This if-elif-else statementes, will evaluate your provided grade, on where it is classfied  whether your failed or did good 
 # during your semester. 
-if GradeMoRoundedOff >= 65 and Grade <= 75:
+if GradeMoRoundedOff >= 65 and GradeMoRoundedOff <= 75:
     print("5.0")
     print("Failure")
-elif Grades == 75:
+elif GradeMoRoundedOff == 75:
     print("3.0")
     print("Passing")
-elif Grades >= 76 and Grades <= 78:
+elif GradeMoRoundedOff >= 76 and GradeMoRoundedOff <= 78:
     print("2.75")
     print("Satisfactory")
-elif Grades >= 79 and Grades <= 81:
+elif GradeMoRoundedOff >= 79 and GradeMoRoundedOff <= 81:
     print("2.5")
     print("Satisfactory")
-elif Grades >= 82 and Grades <= 84:
+elif GradeMoRoundedOff >= 82 and GradeMoRoundedOff <= 84:
     print("2.25")
     print("Good")
-elif Grades >= 85 and Grades <= 87:
+elif GradeMoRoundedOff >= 85 and GradeMoRoundedOff <= 87:
     print("2.0")
     print("Good")
-elif Grades >= 88 and Grades <= 90:
+elif GradeMoRoundedOff >= 88 and GradeMoRoundedOff <= 90:
     print("1.75")
     print("Very Good")
-elif Grades >= 91 and Grades <= 93:
+elif GradeMoRoundedOff >= 91 and GradeMoRoundedOff <= 93:
     print("1.5")
     print("Very Good")
-elif Grades >= 94 and Grades <= 96:
+elif GradeMoRoundedOff >= 94 and GradeMoRoundedOff <= 96:
     print("1.25")
     print("Excellent")
-elif Grades >= 97 and Grades <= 100:
+elif GradeMoRoundedOff >= 97 and GradeMoRoundedOff <= 100:
     print("1.0")
     print("Excellent")
     
