18ffdd2b66b46a782e311266491e3328f2f48356
Ace Lawrence <clavanoace@gmail.com>
2021-11-16 19:30:50 -0800

    Tinatry po iedit ng kaunti mga programs ko

--- LifeStage.py
+++ LifeStage.py
@@ -8,7 +8,7 @@ if IlangTaonKaNa >= 0 and  IlangTaonKaNa <= 12:
 elif IlangTaonKaNa >= 13 and IlangTaonKaNa <= 17:
     print("Teen")
 elif IlangTaonKaNa == 18:
-    print("Debut")    
+    print("Legal Age")    
 elif IlangTaonKaNa >= 19:
     print("Adult")
     
