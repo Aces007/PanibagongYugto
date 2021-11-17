de330c0d733074c31dbec934aae5fbc3f3c73e0e
Ace Lawrence <clavanoace@gmail.com>
2021-11-16 18:19:07 -0800

    currently editing the programs po

--- LifeStage.py
+++ LifeStage.py
@@ -1,5 +1,8 @@
+# This will require you to input your curent age since the beginning of 2021. 
 IlangTaonKaNa = int(input("How old are you?:"))
 
+
+# Then with this if-elif statements, my program will present you what stage in life you're currently in.
 if IlangTaonKaNa >= 0 and  IlangTaonKaNa <= 12:
     print("Kid")
 elif IlangTaonKaNa >= 13 and IlangTaonKaNa <= 17:
